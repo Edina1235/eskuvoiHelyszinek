3606c82a3ced8c27862cf9f3c19ba0ef3b8a66fe
Edina1235 <100123047+Edina1235@users.noreply.github.com>
2023-05-05 20:13:12 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   firebase: {
-    projectId: 'eskuvoihelyszinek-645b0',
-    appId: '1:757732339580:web:96937872c6a981ec557ef9',
-    storageBucket: 'eskuvoihelyszinek-645b0.appspot.com',
-    apiKey: 'AIzaSyCmZ-EJQllK_XZkcefIudPuvjUUBSsQnm8',
-    authDomain: 'eskuvoihelyszinek-645b0.firebaseapp.com',
-    messagingSenderId: '757732339580',
-    measurementId: 'G-C3PQZVGPYD',
+    ...
   },
   production: false,
   hostUrl: 'http://localhost:4200'
