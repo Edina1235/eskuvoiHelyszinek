89951f59f0307eda18c815b64c39f51cf76e00e7
Edina1235 <100123047+Edina1235@users.noreply.github.com>
2023-05-06 07:27:08 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
 export const environment = {
   firebase: {
-    ...
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
   production: false,
   hostUrl: 'http://localhost:4200'
